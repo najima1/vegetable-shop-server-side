137747402d3dda7bdb226dec20321aee6ec0b668
najima1 <mohammednazim3629@gmail.com>
2022-11-09 08:32:19 -0600

    checking

--- controlar.js
+++ controlar.js
@@ -70,18 +70,18 @@ const getSingleProduct = async (req, res) => {
 // make user review 
 const makeUserReview = async (req, res) => {
     try {
-        const { name, email, msg, img_url, productID } = req.body
+        const { name, email, msg, image, id } = req.body
         console.log(name, email, id);
 
 
-        if (!name && !email && !msg && !img_url && !productID) {
+        if (!name && !email && !msg && !image && !id) {
             return res.send({
                 status: false,
                 message: "review value not found"
             })
         }
 
-        const validReview = { name, email, msg, img_url, productID }
+        const validReview = { name, email, msg, image, id }
         const result = await user_review.insertOne(validReview)
 
         if (validReview) {
